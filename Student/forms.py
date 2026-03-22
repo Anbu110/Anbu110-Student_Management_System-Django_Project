4f4d5861ea7af17a58655faee07fd90dd40629ba
Anbumani <anbumani1102003@gmail.com>
2026-03-22 13:43:10 +0530

    added date picker

--- Student/forms.py
+++ Student/forms.py
@@ -1,20 +1,31 @@
-# from django.forms import ModelForm
+       
 from django import forms
 from .models import *
 
 class Student_Form(forms.ModelForm):
     
     class Meta:
-        model=StudentModel
-        fields='__all__'
+        model = StudentModel
+        fields = '__all__'
         
-        widgets={
-            'Name':forms.TextInput(attrs={'class':'form-control'}),
-            'Age':forms.NumberInput(attrs={'class':'form-control'}),
-            'Department': forms.Select(attrs={'class':'form-control'}), 
-            'Email':forms.EmailInput(attrs={'class':'form-control'}),
-            'Join_Date':forms.DateInput(attrs={'class':'form-control'}),
-
-            
+        widgets = {
+            'Name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter student name'
+            }),
+            'Age': forms.NumberInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter age'
+            }),
+            'Department': forms.Select(attrs={
+                'class': 'form-control'
+            }),
+            'Email': forms.EmailInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter email address'
+            }),
+            'Join_Date': forms.DateInput(attrs={
+                'class': 'form-control',
+                'type': 'date'   
+            }),
         }
-        
